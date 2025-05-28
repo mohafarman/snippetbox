39df5c44013b9a2f248afc649f0ae6743871c72c
mohafarman <mohamadfarman977@gmail.com>
2025-05-28 03:06:55 +0200

    disabled fileserver directory listings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log/slog"
 	"net/http"
+	"path/filepath"
 	"time"
 )
 
@@ -14,8 +15,9 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	/* TODO: Disable directory listings */
-	fs := http.FileServer(http.Dir("./ui/static/"))
+	/* INFO: http.FileServer will transform os.ErrNotExist from
+	   neuteredFS.Open() to 404 Not Found response. */
+	fs := http.FileServer(neuteredFS{http.Dir("./ui/static/")})
 	mux.Handle("/static/", http.StripPrefix("/static", fs))
 
 	mux.HandleFunc("/", home)
@@ -33,4 +35,35 @@ func main() {
 	slog.Info("Listening on port " + PORT)
 	err := server.ListenAndServe()
 	slog.Error("Server failed to run. Error: " + err.Error())
+}
+
+type neuteredFS struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFS) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			/* INFO: Will return an os.ErrNotExist. */
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
