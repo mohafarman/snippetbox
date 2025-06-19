5d2348a2dbb1a9b69e6aa6845b51a209beee8da9
mohafarman <mohamadfarman977@gmail.com>
2025-06-19 14:18:37 +0200

    flash message to confirm new snippet creation to user

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -14,11 +14,14 @@ type templateData struct {
 	Snippets    []*models.Snippet
 	CurrentYear int
 	Form        any
+	Flash       string
 }
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
+		/* Is added if the "flash" key exists */
+		Flash: app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
